95e92cd6ae2b90ebb79873ba9de0e104894bc59a
Leo <leoaromero2015@gmail.com>
2020-12-05 20:24:52 -0300

    segundo paso

--- apps/appweb/api/api.py
+++ apps/appweb/api/api.py
@@ -1,10 +1,25 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from apps.appweb.models import Proveedor
 from apps.appweb.api.serializers import ProveedorSerializer
 
-class ProveedorAPIView(APIView):
-    def get(self, request):
+from rest_framework.decorators import api_view                          # para implementar un def
+
+# class ProveedorAPIView(APIView):
+#     def get(self, request):
+#         proveed = Proveedor.objects.all()
+#         proveed_serializer = ProveedorSerializer(proveed, many=True)    # porque son varios
+#         return Response(proveed_serializer.data)
+
+@api_view(['GET', 'POST'])
+def proveed_api_view(request):
+    if request.method == 'GET':
         proveed = Proveedor.objects.all()
         proveed_serializer = ProveedorSerializer(proveed, many=True)    # porque son varios
         return Response(proveed_serializer.data)
+    elif request.method == 'POST':
+        proveed_serializer = ProveedorSerializer(data = request.data)
+        if proveed_serializer.is_valid():
+            proveed_serializer.save()
+            return Response(proveed_serializer.data)
+        return Response(proveed_serializer.errors)
--- apps/appweb/api/urls.py
+++ apps/appweb/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.appweb.api.api import ProveedorAPIView
+from apps.appweb.api.api import proveed_api_view #ProveedorAPIView
 
 urlpatterns = [
-    path('proveedor/', ProveedorAPIView.as_view(), name='proveed_api')
+   # path('proveedor/', ProveedorAPIView.as_view(), name='proveed_api')
+    path('proveedor/', proveed_api_view, name='proveed_api')
 ]
